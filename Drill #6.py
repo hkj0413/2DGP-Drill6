e88f3c9aa267ce9f0fc585f683bdb53a934324ae
hkj0413 <60205654+hkj0413@users.noreply.github.com>
2024-10-04 15:44:44 +0900

    배경 그리기

--- Drill #6.py
+++ Drill #6.py
@@ -1,9 +1,25 @@
 from pico2d import *
 
-open_canvas()
-ground = load_image('TUK_GROUND.png')
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+
+TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 
 def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
     pass
 
+running = True
+
+while running:
+    clear_canvas()
+    TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    update_canvas()
+    handle_events()
+
+close_canvas()
